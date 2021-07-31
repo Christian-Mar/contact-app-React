3aab89cd08878ebc0fb7ae146f02bab04f1898b4
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-07-31 12:44:21 +0200

    Commit with useState

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React from 'react';
 
 class AddContact extends React.Component {
-  render() {
-    return (
+	state = {
+		name: '',
+		email: '',
+	};
+
+    add = (e) => {
+      e.preventDefault(); //code dient pas uitgevoerd te worden na button te gebruiken
+      if(this.state.name === "" || this.state.email === "") {
+        alert ("All the fields are mandatory!");
+        return
+      }
+      this.props.addContactHandler(this.state);
+      this.setState({ name: "", email: "" }); //to clear
+    }
+	render() {
+		return (
 			<div className="ui main">
-				
-				<form className="ui form">
-        <h2>Add Contact</h2>
+				<form className="ui form" onSubmit={this.add}>
+					<h2>Add Contact</h2>
 					<div className="field">
 						<label>Name</label>
-						<input type="text" name="name" placeholder="name" />
+						<input
+							type="text"
+							name="name"
+							placeholder="name"
+							value={this.state.name}
+							onChange={e => this.setState({ name: e.target.value })}
+						/>
 					</div>
 					<div className="field">
 						<label>Email</label>
-						<input type="email" name="email" placeholder="Name@..." />
+						<input
+							type="email"
+							name="email"
+							placeholder="Name@..."
+							value={this.state.email}
+							onChange={e => this.setState({ email: e.target.value })}
+						/>
 					</div>
-          <button className="ui button blue">Add</button>
+					<button className="ui button blue">Add</button>
 				</form>
 			</div>
 		);
-  }
+	}
 }
 
 export default AddContact;
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -6,23 +6,24 @@ import ContactList from "./ContactList";
 
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "John",
-      email: "john@telenet.be"
-  },
-  {
-      id: "2",
-      name: "Anna",
-      email: "anna@telenet.be"
-  }
+  const LOCAL_STORAGE_KEY="contacts";
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) =>
+  { console.log(contact);
+    setContacts([...contacts, contact]);
+  };
 
-  ]
+  useEffect(() => {
+		const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+	}, []);
+
+
+  useEffect(() => {localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));}, [contacts]); // zorgt voor local storage in de browser
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/> 
     </div>
   );
