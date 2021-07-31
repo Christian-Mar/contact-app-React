0db62a892d30a4c0cd5bc76144c011d7b41654fc
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-07-31 13:29:59 +0200

    Commit basic app functionality finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import { uuid } from "uuidv4";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -6,12 +7,22 @@ import ContactList from "./ContactList";
 
 
 function App() {
+  
   const LOCAL_STORAGE_KEY="contacts";
   const [contacts, setContacts] = useState([]);
+  
   const addContactHandler = (contact) =>
   { console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   };
+
+  const removeContactHandler = (id => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  });
 
   useEffect(() => {
 		const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
@@ -24,7 +35,7 @@ function App() {
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts}/> 
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/> 
     </div>
   );
 }
